bbf3b48c3615357383d88ae788c90593be7959d2
Jan Balangue <jan.a.balangue@gmail.com>
2020-12-04 19:57:53 -0600

    Modify BreweryClient and BreweryClientTest; add CustomerClient and CustomerClientTest.

---
+++ src/main/java/io/github/computeruser88/breweryclient/web/client/CustomerClient.java
@@ -0,0 +1,47 @@
+package io.github.computeruser88.breweryclient.web.client;
+
+import io.github.computeruser88.breweryclient.web.model.CustomerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.UUID;
+
+/**
+ * Created by Jan Balangue on 12/4/20.
+ */
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class CustomerClient {
+
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public CustomerClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public CustomerDto getCustomerById(UUID customerId) {
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + customerId.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + "/" + customerId.toString(), customerDto);
+    }
+
+    public void deleteCustomer(UUID customerId) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + customerId);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
---
+++ src/test/java/io/github/computeruser88/breweryclient/web/client/CustomerClientTest.java
@@ -0,0 +1,42 @@
+package io.github.computeruser88.breweryclient.web.client;
+
+import io.github.computeruser88.breweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomerClientTest {
+
+    @Autowired
+    CustomerClient client;
+
+    @Test
+    void testGetCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().build();
+        URI uri = client.saveNewCustomer(customerDto);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
+    }
+}
